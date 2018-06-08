ea2b84660a1c246805bc7e59a0a160581c1fd06b
sasankdas <alladisasankdas@gmail.com>
2018-06-08 17:21:16 -0400

    looping through our books details in book-list template

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, Input} from "@angular/core";
+import { Book } from "../shared/book";
 
 @Component({
   selector: 'app-book-list',
@@ -7,6 +8,8 @@ import {Component, OnInit} from "@angular/core";
 })
 export class BookListComponent implements OnInit {
 
+@Input()
+  private books: Book[];
   ngOnInit() {
   }
 
